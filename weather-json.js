b70a926aaa9efa16e8f41bae9e5264821171ba94
Rakim Ali <rakimali770@gmail.com>
2024-05-03 09:20:40 +0600

    weather json.js

---
+++ weather-json.js
@@ -0,0 +1,567 @@
+let weather = {   //object 1
+    "message": "accurate",
+    "cod": "200",
+    "count": 15,
+    "list": [     //array 1
+      {           //arr 1 obj 1 index 0
+        "id": 2636389,
+        "name": "Swindon",
+        "coord": {
+          "lat": 51.558,
+          "lon": -1.7812
+        },
+        "main": {
+          "temp": 6.75,
+          "pressure": 1008,
+          "humidity": 72,
+          "temp_min": 6,
+          "temp_max": 7
+        },
+        "dt": 1516197360,
+        "wind": {
+          "speed": 9.8,
+          "deg": 260,
+          "gust": 11.8
+        },
+        "sys": {
+          "country": ""
+        },
+        "rain": null,
+        "snow": null,
+        "clouds": {
+          "all": 75
+        },
+        "weather": [
+          {
+            "id": 521,
+            "main": "Rain",
+            "description": "proximity shower rain",
+            "icon": "09d"
+          }
+        ]
+      },
+      {           //arr 1 obj 2 index 1
+        "id": 3333207,
+        "name": "Borough of Swindon",
+        "coord": {
+          "lat": 51.5833,
+          "lon": -1.75
+        },
+        "main": {
+          "temp": 7,
+          "pressure": 1008,
+          "humidity": 72,
+          "temp_min": 7,
+          "temp_max": 7
+        },
+        "dt": 1516197360,
+        "wind": {
+          "speed": 9.8,
+          "deg": 260,
+          "gust": 11.8
+        },
+        "sys": {
+          "country": ""
+        },
+        "rain": null,
+        "snow": null,
+        "clouds": {
+          "all": 75
+        },
+        "weather": [
+          {
+            "id": 521,
+            "main": "Rain",
+            "description": "proximity shower rain",
+            "icon": "09d"
+          }
+        ]
+      },
+      {           //arr 1 obj 3 index 2
+        "id": 2654657,
+        "name": "Broad Blunsdon",
+        "coord": {
+          "lat": 51.6134,
+          "lon": -1.7787
+        },
+        "main": {
+          "temp": 7,
+          "pressure": 1008,
+          "humidity": 72,
+          "temp_min": 7,
+          "temp_max": 7
+        },
+        "dt": 1516197360,
+        "wind": {
+          "speed": 9.8,
+          "deg": 260,
+          "gust": 11.8
+        },
+        "sys": {
+          "country": ""
+        },
+        "rain": null,
+        "snow": null,
+        "clouds": {
+          "all": 75
+        },
+        "weather": [
+          {
+            "id": 521,
+            "main": "Rain",
+            "description": "proximity shower rain",
+            "icon": "09d"
+          }
+        ]
+      },
+      {           //arr 1 obj 4 index 3
+        "id": 2633476,
+        "name": "Wroughton",
+        "coord": {
+          "lat": 51.5241,
+          "lon": -1.7956
+        },
+        "main": {
+          "temp": 6.75,
+          "pressure": 1008,
+          "humidity": 72,
+          "temp_min": 6,
+          "temp_max": 7
+        },
+        "dt": 1516197360,
+        "wind": {
+          "speed": 9.8,
+          "deg": 260,
+          "gust": 11.8
+        },
+        "sys": {
+          "country": ""
+        },
+        "rain": null,
+        "snow": null,
+        "clouds": {
+          "all": 75
+        },
+        "weather": [
+          {
+            "id": 521,
+            "main": "Rain",
+            "description": "proximity shower rain",
+            "icon": "09d"
+          }
+        ]
+      },
+      {           //arr 1 obj 5 index 4
+        "id": 2634815,
+        "name": "Wanborough",
+        "coord": {
+          "lat": 51.5426,
+          "lon": -1.6984
+        },
+        "main": {
+          "temp": 7,
+          "pressure": 1008,
+          "humidity": 72,
+          "temp_min": 7,
+          "temp_max": 7
+        },
+        "dt": 1516197360,
+        "wind": {
+          "speed": 9.8,
+          "deg": 260,
+          "gust": 11.8
+        },
+        "sys": {
+          "country": ""
+        },
+        "rain": null,
+        "snow": null,
+        "clouds": {
+          "all": 75
+        },
+        "weather": [
+          {
+            "id": 521,
+            "main": "Rain",
+            "description": "proximity shower rain",
+            "icon": "09d"
+          }
+        ]
+      },
+      {           //arr 1 obj 6 index 5
+        "id": 2643321,
+        "name": "Lydiard Tregoze",
+        "coord": {
+          "lat": 51.55,
+          "lon": -1.85
+        },
+        "main": {
+          "temp": 7,
+          "pressure": 1008,
+          "humidity": 72,
+          "temp_min": 7,
+          "temp_max": 7
+        },
+        "dt": 1516197360,
+        "wind": {
+          "speed": 9.8,
+          "deg": 260,
+          "gust": 11.8
+        },
+        "sys": {
+          "country": ""
+        },
+        "rain": null,
+        "snow": null,
+        "clouds": {
+          "all": 75
+        },
+        "weather": [
+          {
+            "id": 521,
+            "main": "Rain",
+            "description": "proximity shower rain",
+            "icon": "09d"
+          }
+        ]
+      },
+      {           //arr 1 obj 7 index 6
+        "id": 7291961,
+        "name": "Wroughton",
+        "coord": {
+          "lat": 51.5178,
+          "lon": -1.8056
+        },
+        "main": {
+          "temp": 6.75,
+          "pressure": 1008,
+          "humidity": 72,
+          "temp_min": 6,
+          "temp_max": 7
+        },
+        "dt": 1516197360,
+        "wind": {
+          "speed": 9.8,
+          "deg": 260,
+          "gust": 11.8
+        },
+        "sys": {
+          "country": ""
+        },
+        "rain": null,
+        "snow": null,
+        "clouds": {
+          "all": 75
+        },
+        "weather": [
+          {
+            "id": 521,
+            "main": "Rain",
+            "description": "proximity shower rain",
+            "icon": "09d"
+          }
+        ]
+      },
+      {           //arr 1 obj 8 index 7
+        "id": 6696027,
+        "name": "Chiseldon",
+        "coord": {
+          "lat": 51.5161,
+          "lon": -1.7321
+        },
+        "main": {
+          "temp": 7,
+          "pressure": 1008,
+          "humidity": 72,
+          "temp_min": 7,
+          "temp_max": 7
+        },
+        "dt": 1516197360,
+        "wind": {
+          "speed": 9.8,
+          "deg": 260,
+          "gust": 11.8
+        },
+        "sys": {
+          "country": ""
+        },
+        "rain": null,
+        "snow": null,
+        "clouds": {
+          "all": 75
+        },
+        "weather": [
+          {
+            "id": 521,
+            "main": "Rain",
+            "description": "proximity shower rain",
+            "icon": "09d"
+          }
+        ]
+      },
+      {           //arr 1 obj 9 index 8
+        "id": 2639840,
+        "name": "Purton",
+        "coord": {
+          "lat": 51.5833,
+          "lon": -1.8667
+        },
+        "main": {
+          "temp": 7,
+          "pressure": 1008,
+          "humidity": 72,
+          "temp_min": 7,
+          "temp_max": 7
+        },
+        "dt": 1516197360,
+        "wind": {
+          "speed": 9.8,
+          "deg": 260,
+          "gust": 11.8
+        },
+        "sys": {
+          "country": ""
+        },
+        "rain": null,
+        "snow": null,
+        "clouds": {
+          "all": 75
+        },
+        "weather": [
+          {
+            "id": 521,
+            "main": "Rain",
+            "description": "proximity shower rain",
+            "icon": "09d"
+          }
+        ]
+      },
+      {           //arr 1 obj 10 index 9
+        "id": 2647519,
+        "name": "Hannington",
+        "coord": {
+          "lat": 51.6333,
+          "lon": -1.75
+        },
+        "main": {
+          "temp": 7,
+          "pressure": 1008,
+          "humidity": 72,
+          "temp_min": 7,
+          "temp_max": 7
+        },
+        "dt": 1516197360,
+        "wind": {
+          "speed": 9.8,
+          "deg": 260,
+          "gust": 11.8
+        },
+        "sys": {
+          "country": ""
+        },
+        "rain": null,
+        "snow": null,
+        "clouds": {
+          "all": 75
+        },
+        "weather": [
+          {
+            "id": 521,
+            "main": "Rain",
+            "description": "proximity shower rain",
+            "icon": "09d"
+          }
+        ]
+      },
+      {           //arr 1 obj 11 index 10
+        "id": 2646916,
+        "name": "Highworth",
+        "coord": {
+          "lat": 51.6305,
+          "lon": -1.711
+        },
+        "main": {
+          "temp": 7,
+          "pressure": 1008,
+          "humidity": 72,
+          "temp_min": 7,
+          "temp_max": 7
+        },
+        "dt": 1516197360,
+        "wind": {
+          "speed": 9.8,
+          "deg": 260,
+          "gust": 11.8
+        },
+        "sys": {
+          "country": ""
+        },
+        "rain": null,
+        "snow": null,
+        "clouds": {
+          "all": 75
+        },
+        "weather": [
+          {
+            "id": 521,
+            "main": "Rain",
+            "description": "proximity shower rain",
+            "icon": "09d"
+          }
+        ]
+      },
+      {           //arr 1 obj 12 index 11
+        "id": 2637889,
+        "name": "Shrivenham",
+        "coord": {
+          "lat": 51.5985,
+          "lon": -1.6547
+        },
+        "main": {
+          "temp": 7,
+          "pressure": 1008,
+          "humidity": 72,
+          "temp_min": 7,
+          "temp_max": 7
+        },
+        "dt": 1516197360,
+        "wind": {
+          "speed": 9.8,
+          "deg": 260,
+          "gust": 11.8
+        },
+        "sys": {
+          "country": ""
+        },
+        "rain": null,
+        "snow": null,
+        "clouds": {
+          "all": 75
+        },
+        "weather": [
+          {
+            "id": 521,
+            "main": "Rain",
+            "description": "proximity shower rain",
+            "icon": "09d"
+          }
+        ]
+      },
+      {           //arr 1 obj 13 index 12
+        "id": 2655553,
+        "name": "Bishopstone",
+        "coord": {
+          "lat": 51.5513,
+          "lon": -1.6471
+        },
+        "main": {
+          "temp": 7,
+          "pressure": 1008,
+          "humidity": 72,
+          "temp_min": 7,
+          "temp_max": 7
+        },
+        "dt": 1516197360,
+        "wind": {
+          "speed": 9.8,
+          "deg": 260,
+          "gust": 11.8
+        },
+        "sys": {
+          "country": ""
+        },
+        "rain": null,
+        "snow": null,
+        "clouds": {
+          "all": 75
+        },
+        "weather": [
+          {
+            "id": 521,
+            "main": "Rain",
+            "description": "proximity shower rain",
+            "icon": "09d"
+          }
+        ]
+      },
+      {           //arr 1 obj 14 index 13
+        "id": 2633571,
+        "name": "Royal Wootton Bassett",
+        "coord": {
+          "lat": 51.5419,
+          "lon": -1.9045
+        },
+        "main": {
+          "temp": 7,
+          "pressure": 1008,
+          "humidity": 72,
+          "temp_min": 7,
+          "temp_max": 7
+        },
+        "dt": 1516197360,
+        "wind": {
+          "speed": 9.8,
+          "deg": 260,
+          "gust": 11.8
+        },
+        "sys": {
+          "country": ""
+        },
+        "rain": null,
+        "snow": null,
+        "clouds": {
+          "all": 75
+        },
+        "weather": [
+          {
+            "id": 521,
+            "main": "Rain",
+            "description": "proximity shower rain",
+            "icon": "09d"
+          }
+        ]
+      },
+      {           //arr 1 obj 15 index 14
+        "id": 2651985,
+        "name": "Cricklade",
+        "coord": {
+          "lat": 51.6406,
+          "lon": -1.8574
+        },
+        "main": {
+          "temp": 7,
+          "pressure": 1008,
+          "humidity": 72,
+          "temp_min": 7,
+          "temp_max": 7
+        },
+        "dt": 1516197360,
+        "wind": {
+          "speed": 9.8,
+          "deg": 260,
+          "gust": 11.8
+        },
+        "sys": {
+          "country": ""
+        },
+        "rain": null,
+        "snow": null,
+        "clouds": {
+          "all": 75
+        },
+        "weather": [
+          {
+            "id": 521,
+            "main": "Rain",
+            "description": "proximity shower rain",
+            "icon": "09d"
+          }
+        ]
+      }
+    ]
+  }
+  
+  console.log(weather.list[0].name);
+  console.log(weather.list[0].main);
+  console.log(weather.list[0].main.pressure);
+  console.log(weather.list[5].coord.lat);
